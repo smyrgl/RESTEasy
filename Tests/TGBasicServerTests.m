0b4c00eca9b6fcbefd682bd72107526c0c4ab5c7
John Tumminaro <john@tinylittlegears.com>
2014-04-26 15:59:24 -0700

    Updated tests so that server start/stop tests wait for the server notification.

--- Tests/TGBasicServerTests.m
+++ Tests/TGBasicServerTests.m
@@ -31,15 +31,30 @@
 - (void)testStartServer
 {
     XCTAssert(![[TGRESTServer sharedServer] isRunning], @"Server must not be running");
+    __weak typeof(self) weakSelf = self;
+    [[NSNotificationCenter defaultCenter] addObserverForName:TGRESTServerDidStartNotification
+                                                      object:nil
+                                                       queue:nil
+                                                  usingBlock:^(NSNotification *note) {
+                                                      [weakSelf notify:XCTAsyncTestCaseStatusSucceeded];
+                                                  }];
     [[TGRESTServer sharedServer] startServerWithOptions:nil];
-    
+    [self waitForTimeout:1];
     XCTAssert([[TGRESTServer sharedServer] isRunning], @"Server must be running");
 }
 
 - (void)testStopServer
 {
     XCTAssert(![[TGRESTServer sharedServer] isRunning], @"Server must not be running");
+    __weak typeof(self) weakSelf = self;
+    [[NSNotificationCenter defaultCenter] addObserverForName:TGRESTServerDidStartNotification
+                                                      object:nil
+                                                       queue:nil
+                                                  usingBlock:^(NSNotification *note) {
+                                                      [weakSelf notify:XCTAsyncTestCaseStatusSucceeded];
+                                                  }];
     [[TGRESTServer sharedServer] startServerWithOptions:nil];
+    [self waitForTimeout:1];
     XCTAssert([[TGRESTServer sharedServer] isRunning], @"Server must be running");
     [[TGRESTServer sharedServer] stopServer];
     XCTAssert(![[TGRESTServer sharedServer] isRunning], @"Server must not be running");
