fa2570371d1f220f7e468671060cb4c08dfce2a6
John Tumminaro <john@tinylittlegears.com>
2014-04-26 23:35:00 -0700

    Documented headers for concrete store classes.

--- Classes/TGRESTSqliteStore.h
+++ Classes/TGRESTSqliteStore.h
@@ -9,6 +9,12 @@
 #import <Foundation/Foundation.h>
 #import "TGRESTStore.h"
 
+/**
+ *  Concrete subclass of TGRESTStore, this store type uses a sqlite3 database as the backing store offering a measure of persistence.  However this is still not meant for anything permanent which is reflected in the fact that this store will drop the schema and recreate the db anytime a resource model doesn't match the existing tables.  
+ 
+    Note that the sqlite database is configured with FOREIGN KEY support so the performance of relational queries that are one-to-many should be pretty reasonable. 
+ */
+
 @interface TGRESTSqliteStore : TGRESTStore
 
 @end
